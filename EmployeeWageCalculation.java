75438a16c4a02682e02898a655c6e9e2063a05ec
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-03 01:27:19 -0700

    UC-1 added

---
+++ EmployeeWageCalculation.java
@@ -0,0 +1,12 @@
+public class EmployeeWageCalculation{
+
+	public static void main (String[] args) {
+
+		int Is_Present =1;
+	double empCheck = Math.floor(Math.random() *10) %2;
+	if( empCheck == Is_Present )
+		System.out.println("IS PRESENT");
+	else
+		System.out.println("IS ABSENT");
+	}
+	}

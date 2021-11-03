df1adcb10c4fd7f8b12143e7c8bdf7b899839df3
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-03 01:29:58 -0700

    UC-2 added

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -1,12 +1,20 @@
 public class EmployeeWageCalculation{
 
-	public static void main (String[] args) {
+   public static void main (String[] args) {
 
-		int Is_Present =1;
-	double empCheck = Math.floor(Math.random() *10) %2;
-	if( empCheck == Is_Present )
-		System.out.println("IS PRESENT");
-	else
-		System.out.println("IS ABSENT");
-	}
-	}
+      int Is_Present =1;
+      int Emp_Rate_Per_Hour=20;
+      int Emp_Daily_Hours=8;
+      int DailyWage=0;
+   double empCheck = Math.floor(Math.random() *10) %2;
+   if ( empCheck == Is_Present ){
+         Emp_Daily_Hours=8;
+   
+      DailyWage=Emp_Daily_Hours*Emp_Rate_Per_Hour;
+         System.out.println("IS PRESENT" +DailyWage);}
+         else{
+         Emp_Daily_Hours=0;
+      System.out.println("IS ABSENT");
+   }
+}
+   }

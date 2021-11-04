ed35e87c1d5616ea652e454482980d3dc94f8849
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-03 22:19:09 -0700

    UC-4 added

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -1,24 +1,23 @@
 public class EmployeeWageCalculation{
+		public static final int Is_Fulltime =1;
+      public static final int Is_Parttime =2;
+      public static final int empRatePerHour=20;
+			 public static void main (String[] args) {
+      int empHours=0;
+      int empWage=0;
+   int empCheck = (int) Math.floor(Math.random() *10) %3;
 
-	public static void main (String[] args) {
-		
-		int Is_Fulltime =1;
-		int Is_Parttime =2;
-		int empRatePerHour=20;
-		int empHours=0;
-		int empWage=0;
-	double empCheck = Math.floor(Math.random() *10) %3;
-
-	if( empCheck == Is_Fulltime ) {
-		empHours=4;
-		System.out.println("IS PARTTIME" + empWage);}
-	else if (empCheck == Is_Parttime ) {
-		empHours=8;
-		System.out.println("IS FULLTIME" + empWage);}
-		else  {
-		empHours=0;
-		empWage= empHours*empRatePerHour;
-		System.out.println("IS ABSENT" + empWage);
+   switch (empCheck) {
+         case Is_Fulltime:
+            empHours=8;
+               break;
+         case Is_Parttime:
+            empHours=4;
+            break;
+         default:
+            empHours=0;
 }
-	}
-	}
+      empWage = empHours*empRatePerHour;
+         System.out.println("empWage" +empWage);
+}
+}

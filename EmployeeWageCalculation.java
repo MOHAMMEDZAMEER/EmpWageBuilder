b81a24cdf9a81c6e087ab4ca41cb967549884cdd
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-03 22:26:29 -0700

    UC-5 added

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -1,11 +1,14 @@
 public class EmployeeWageCalculation{
-		public static final int Is_Fulltime =1;
+      public static final int Is_Fulltime =1;
       public static final int Is_Parttime =2;
       public static final int empRatePerHour=20;
-			 public static void main (String[] args) {
+	public static final int NumofWorkingDays=2;
+          public static void main (String[] args) {
       int empHours=0;
       int empWage=0;
-   int empCheck = (int) Math.floor(Math.random() *10) %3;
+	int totalempWage=0;
+	for (int day=0;day<NumofWorkingDays;day++){
+   int  empCheck = (int) Math.floor(Math.random() *10) %3;
 
    switch (empCheck) {
          case Is_Fulltime:
@@ -16,8 +19,12 @@ public class EmployeeWageCalculation{
             break;
          default:
             empHours=0;
+				break;
 }
       empWage = empHours*empRatePerHour;
+	totalempWage +=empWage;
          System.out.println("empWage" +empWage);
+}
+	System.out.println("totalempWage: " + totalempWage);
 }
 }

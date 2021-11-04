dc33093a1e157682f4ce96f53e82d4ec0c5e676b
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-03 23:26:10 -0700

    UC-6 added

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -3,28 +3,31 @@ public class EmployeeWageCalculation{
       public static final int Is_Parttime =2;
       public static final int empRatePerHour=20;
 	public static final int NumofWorkingDays=2;
+	public static final int MaxHoursinaMonth=10;
           public static void main (String[] args) {
       int empHours=0;
       int empWage=0;
-	int totalempWage=0;
-	for (int day=0;day<NumofWorkingDays;day++){
-   int  empCheck = (int) Math.floor(Math.random() *10) %3;
-
-   switch (empCheck) {
-         case Is_Fulltime:
-            empHours=8;
-               break;
-         case Is_Parttime:
-            empHours=4;
-            break;
-         default:
-            empHours=0;
-				break;
+	int totalworkingdays=0;
+	int totalempHours=0;
+	while (totalempHours<=MaxHoursinaMonth&&
+			totalworkingdays<NumofWorkingDays) {
+		totalworkingdays++;
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+        case Is_Fulltime:
+           empHours=8;
+              break;
+        case Is_Parttime:
+           empHours=4;
+           break;
+        default:
+           empHours=0;
 }
-      empWage = empHours*empRatePerHour;
-	totalempWage +=empWage;
-         System.out.println("empWage" +empWage);
+    totalempHours +=empHours;
+    System.out.println("day#:"+totalworkingdays+"empHr:"+empHours);;
+   
 }
-	System.out.println("totalempWage: " + totalempWage);
+	int totalempWage=totalempHours * empRatePerHour;
+	System.out.println("total emp Wage:"+totalempWage);
 }
-}
+	}
